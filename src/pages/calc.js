55a91866cf78c43eea0b7d28fff33d89b12e5d14
yunseopkim2 <dbstjqdlwksj@gmail.com>
2022-03-25 18:19:55 +0900

    2022-03-25 18:19

--- src/pages/calc.js
+++ src/pages/calc.js
@@ -1,46 +1,45 @@
 
-import React,{useState} from 'react' 
-
-
-
-export default function Calc() {
-    const [inputs, setInputs] = useState({})
-    const [result, setResult] = useState('')
-    const {num1, opcode, num2} = inputs;
-
-    const handleChange = (e) => {
-        e.preventDefault()
-        const {value, name} = e.target;
-        setInputs({...inputs, [name]: value})
+import React,{useState} from "react"
+export default function Calc(){
+    const [num1, setNum1] = useState(0)
+    const [opcode, setOpcode] = useState("")
+    const [num2, setNum2] = useState(0)
+    const [result, setResult] = useState(0)
+    const sum = () => {
+        return setResult(Number(num1) + Number(num2))
     }
-    const handleClick = (e) => {
-        e.preventDefault()
-        result = ({})
-        /*memberCalc({num1, opcode ,num2}).then(res => setResult(res.data)).catch( err => console.log(`에러발생 : ${err}`))*/
+    const sub = () => {
+        return setResult(Number(num1) - Number(num2))
     }
-    return <><h1>Calc폼</h1>
-    <form action=""> 
+    const mul = () => {
+        return setResult(Number(num1) * Number(num2))
+    }
+    const div = () => {
+        return setResult(Number(num1) / Number(num2))
+    }
+    return <>
+    <h1>계산기</h1>
+    <div>
     <label><b>num1</b></label>
-    <input name ="num1" type="text" onChange={handleChange} /><br />
-
+    <input id="num1" type="" /><br/>
     <label htmlFor=""><b>opcode</b></label>
-    <select name="opcode" id=""onChange={handleChange}>
-        <option value="+">+</option>
-        <option value="-">-</option>
-        <option value="*">*</option>
-        <option value="/">/</option>
-        <option value="%">%</option>
+    <select name="" id="">
+        <option value="">+</option>
+        <option value="">-</option>
+        <option value="">*</option>
+        <option value="">/</option>
+        <option value="">%</option>
     </select>
-    
-    <br />
-
+    <br/>
     <label htmlFor=""><b>num2</b></label>
-    <input name="num2" type="text" onChange= {handleChange} /><br />
-
-    <button onClick={handleClick}>전 송</button>
-    </form>
-    <div>계산결과 : {result}</div>
-
-
+    <input id="num2" tpye=""/><br/>
+    <button onClick={() => {setNum1(document.getElementById('num1').value)}}>num1 결정</button>
+    <button onClick={() => {setNum2(document.getElementById('num2').value)}}>num2 결정</button><br/>
+    <button onClick={() => {sum()}}>덧셈 실행</button>
+    <button onClick={() => {sub()}}>뺄셈 실행</button><br/>
+    <button onClick={() => {mul()}}>곱셈 실행</button>
+    <button onClick={() => {div()}}>나눗셈 실행</button>
+    <div>계산 결과: {result}</div>
+    </div>
     </>
 }
